62ce577441cba0a68f1e501a8ca3a804278971a1
GabrielNBS <123820384+GabrielNBS@users.noreply.github.com>
2023-08-07 13:15:32 -0300

    adicionando condicional ao text

--- lp-edson/src/components/TextStep.jsx
+++ lp-edson/src/components/TextStep.jsx
@@ -2,15 +2,38 @@
 import React from 'react'
 import "../styles/components/TextStep.sass"
 import HeroOverlay from "../img/background3.jpg"
+import { useEffect, useState } from 'react';
+
 
 function TextStep() {
+    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setWindowWidth(window.innerWidth);
+        };
+
+        // Adicionar um event listener para atualizar o tamanho da tela quando a janela é redimensionada
+        window.addEventListener('resize', handleResize);
+
+        // Remover o event listener ao desmontar o componente
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
     return (
         <section className='step'
-        style={{backgroundImage: `url(${HeroOverlay})`}}>
+            style={{ backgroundImage: `url(${HeroOverlay})` }}>
             <div className="container">
+            {windowWidth < 768 ? (
+                <h2 className="step__text">
+                    impulsione seus <span>resultados</span>.
+                </h2>
+            ) : (
                 <h2 className="step__text">
                     Descubra a excelência no treinamento com um profissional dedicado a <span>impulsionar seus resultados</span>.
                 </h2>
+            )}
             </div>
         </section>
     )
